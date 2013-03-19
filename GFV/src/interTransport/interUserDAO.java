a58fc0ac19f31ae0c19d25d27c477496f8346a90
idan <idannizri5@gmail.com>
2013-03-19 13:15:50 +0000

    user incomplet

--- GFV/src/interTransport/interUserDAO.java
+++ GFV/src/interTransport/interUserDAO.java
@@ -1,13 +1,16 @@
 package interTransport;
+
 import java.util.Collection;
+
 import modelTransport.User;
 import erreur.TransportException;
+
 public interface interUserDAO {
-  
-	public void supprimer(String idUser)throws TransportException; // supprimer (transport, trajet, Itineraire, adresse...) 
-	public String sauvegarde(User p)throws TransportException;// sauvegarde (transport, trajet, Itineraire, adresse...) 
-	public void miseAjourRole(User p)throws TransportException;// mise � jour (transport, trajet, Itineraire, adresse...) 
-	public User chercher(String idUser)throws TransportException;//chercher  (transport, trajet, Itineraire, adresse...) 
+	
+	public void supprimer(User p)throws TransportException; // supprimer (transport, trajet, Itineraire, adresse...) 
+	public void sauvegarde(User p)throws TransportException;// sauvegarde (transport, trajet, Itineraire, adresse...) 
+	public void miseAjour(User p)throws TransportException;// mise � jour (transport, trajet, Itineraire, adresse...) 
+	public User chercher(int id)throws TransportException;//chercher  (transport, trajet, Itineraire, adresse...) 
 	public Collection<User> toutUser()throws TransportException; // chercher  (transport, trajet, Itineraire, adresse...) 
 
 }
